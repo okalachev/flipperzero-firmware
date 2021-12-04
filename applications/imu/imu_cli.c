3d2d8380754dbc4742f880e717fa8f878a57a220
Oleg Kalachev <okalachev@gmail.com>
2021-12-04 04:30:58 +0300

    working imu probing

--- applications/imu/imu_cli.c
+++ applications/imu/imu_cli.c
@@ -4,8 +4,16 @@
 #include <lib/toolbox/args.h>
 #include <cli/cli.h>
 
+#include "mpu6050.h"
+
 void imu_cli(Cli* cli, string_t args, void* context) {
     printf("Hello, IMU!\r\n");
+    if(init_mpu6050()) {
+        printf("IMU OK\r\n");
+    } else {
+        printf("IMU NOT OK\r\n");
+    }
+    release_mpu6050();
 }
 
 void imu_cli_init() {
